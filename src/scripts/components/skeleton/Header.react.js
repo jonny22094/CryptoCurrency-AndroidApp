875f102f27ea6e9a55c706004be7e8f19982e7bc
Jan Rochalski <qwsazxxzaswq@wp.pl>
2018-02-07 14:06:42 +0100

    Icons

--- src/scripts/components/skeleton/Header.react.js
+++ src/scripts/components/skeleton/Header.react.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Icon from 'react-native-vector-icons';
 
 class Header extends Component {
     render() {
--- src/scripts/components/skeleton/List.react.js
+++ src/scripts/components/skeleton/List.react.js
@@ -5,6 +5,7 @@ import cc                                           from "cryptocompare";
 class List extends Component {
 
     state = {
+        name:  "",
         price: "Loading..."
     }
 
@@ -12,21 +13,27 @@ class List extends Component {
         const data = await cc.priceFull(this.props.data.name, this.props.data.curr);
 
         let state = {
+            name: data[this.props.data.name][this.props.data.curr].TOSYMBOL,
             price: data[this.props.data.name][this.props.data.curr].PRICE
         }
 
         this.setState(state);
     }
 
+    componentDidMount() {
+       this.timerID = setInterval(
+         () => this.cryptoPrice(),
+         500
+       );
+    }
 
     render() {
-        this.cryptoPrice();
         return (
             <View style={[skStyles.list, main.verticalCenter, main.bottomB]}>
                 <Text style={[main.text, main.rightB, skStyles.listName]}>{this.props.data.name}</Text>
                 <Text style={[main.text, {flex: 6}]}>
                     {this.state.price}
-                    <Text style={{fontSize: 12}}>{this.props.data.curr}</Text>
+                    <Text style={{fontSize: 12}}>{this.state.name}</Text>
                 </Text>
             </View>
         );
