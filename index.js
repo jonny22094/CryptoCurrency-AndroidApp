3d3307f3e05884b10d678a4e3c104b0eda19e4a4
Jan Rochalski <qwsazxxzaswq@wp.pl>
2018-09-12 13:20:02 +0200

    Android/Ios

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src/scripts/App';
 
-AppRegistry.registerComponent('CryptoCurrencies_AndroidApp', () => App);
+AppRegistry.registerComponent('CryptoCurrenciesApp', () => App);
